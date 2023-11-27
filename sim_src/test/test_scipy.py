bcecf75effff6f243e7f9de309e0f3e5a87c97fb
zhouyou.gu <zhouyou.gu@sydney.edu.au>
2023-11-27 21:44:26 +1100

    test rank-1 sketch

--- sim_src/test/test_scipy.py
+++ sim_src/test/test_scipy.py
@@ -10,10 +10,15 @@ e = env(cell_size=5,seed=int(time.time()))
 a = mmwm_scipy(e.n_sta,e.min_sinr)
 a.set_st(e.rxpr)
 CC = a.run_fc_main(10., num_iterations=100)
+time.sleep(3)
+
 def rank1skretch(A):
-    randv = np.random.randn(A.shape[0],1)
+    d=10000
+    randv = np.random.randn(A.shape[0],d)/math.sqrt(float(d))
     ret = scipy.sparse.linalg.expm_multiply(A.copy(),randv)
     return ret
 
-CC = a.run_fc_exphalffc_plug(10., num_iterations=100*10,exphalffc=rank1skretch)
+CC = a.run_fc_exphalffc_plug_indp(10., num_iterations=100,exphalffc=rank1skretch)
+time.sleep(3)
+CC = a.run_fc_exphalffc_plug(10., num_iterations=100,exphalffc=rank1skretch)
 
