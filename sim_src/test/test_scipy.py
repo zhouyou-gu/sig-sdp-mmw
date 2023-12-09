e896f83b07da4f38b83be8c61a108ca187a24260
zhouyou.gu <zhouyou.gu@sydney.edu.au>
2023-12-09 20:13:22 +1100

    update script

--- sim_src/test/test_scipy.py
+++ sim_src/test/test_scipy.py
@@ -1,3 +1,5 @@
+import os
+
 import math
 import time
 
@@ -6,33 +8,21 @@ import scipy.sparse.linalg
 
 from sim_src.alg.mmwm_scipy import mmwm_scipy
 from sim_src.env.env import env
-from sim_src.util import GLOBAL_PROF_ENABLER
+from sim_src.util import GLOBAL_PROF_ENABLER, plot_a_array
 
-print(np.asarray(5).view())
-
-# GLOBAL_PROF_ENABLER.DISABLE()
-e = env(cell_size=15,seed=0)
+GLOBAL_PROF_ENABLER.DISABLE()
+e = env(cell_size=5,seed=0)
 a = mmwm_scipy(e.n_sta,e.min_sinr)
 a.set_st(e.rxpr)
-# CC = a.run_fc_main(10., num_iterations=100)
-# time.sleep(3)
+a._debug(True,1)
 
-def rank1skretch(A):
-    d=1
+
+def rankdskretch(A, d=1):
     randv = np.random.randn(A.shape[0],d)/math.sqrt(float(d))
     randv = randv/np.linalg.norm(randv,axis=0)
     ret = scipy.sparse.linalg.expm_multiply(A.copy(),randv)
     return ret
 
-# CC = a.run_fc_exphalffc_plug_indp(10., num_iterations=100,exphalffc=rank1skretch)
-# time.sleep(3)
-CC = a.run_fc_exphalffc_plug(26, num_iterations=400,exphalffc=rank1skretch)
+CC = a.run_fc_exphalffc_plug(26, num_iterations=int(1/a.ETA**2)*int(math.log(e.n_sta)),exphalffc=rankdskretch)
 
-
-import matplotlib.pyplot as plt
-data = np.convolve(a.LOGGED_NP_DATA["pct"][:,3], np.ones(50)/50, mode='valid')
-
-plt.plot(a.LOGGED_NP_DATA["pct"][:data.size,0],data)
-# print(a.MOVING_AVERAGE_DICT["pct"])
-print(data.shape)
-plt.show()
+plot_a_array(a.LOGGED_NP_DATA["pct"][:,3],name="pct",script_file=__file__,save_path=os.path.dirname(os.path.realpath(__file__)))
