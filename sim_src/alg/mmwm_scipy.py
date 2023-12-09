d6852d03974846f724eec020c17dbe8bba05b07f
zhouyou.gu <zhouyou.gu@sydney.edu.au>
2023-12-09 20:14:23 +1100

    remove unused codes

--- sim_src/alg/mmwm_scipy.py
+++ sim_src/alg/mmwm_scipy.py
@@ -29,111 +29,6 @@ class mmwm_scipy(feasibility_check_alg,StatusObject):
         self.width_bound = 0.
 
     @profile
-    def run_fc_main(self, Z, num_iterations=None) -> bool:
-        if num_iterations is None:
-            num_iterations = self._get_niteration()
-
-        d_sum = 0
-        d_bst = np.infty
-        X_bst = None
-
-        G = scipy.sparse.csr_matrix((self.K, self.K))
-        for i in range(num_iterations):
-            # compute X from G
-            G_2 = G.copy()
-            G_2.data = G_2.data/2.
-            e_half = np.asarray(scipy.sparse.linalg.expm(G_2.tocsc()).todense())
-            gr, p = vec_rand_rounding.get_group_vec_using_ehalf_nattempt(Z,G_2.copy(),self.rxpr,self.I_max)
-            self._add_np_log("pct",np.asarray([p, p]),g_step=i)
-            X_mdiag_data = np.sum(e_half * e_half, axis=1)
-            X_trace = np.sum(X_mdiag_data)/self.K
-            X_mdiag_data = X_mdiag_data/X_trace
-            X_mdiag = scipy.sparse.diags(X_mdiag_data).tocsr()
-            X_mdiag.sort_indices()
-            X_offdi_data = np.sum(e_half[self.H_x] * e_half[self.H_y], axis=1)/X_trace
-            X_offdi = scipy.sparse.coo_matrix((X_offdi_data, (self.H_x, self.H_y)), shape=(self.K, self.K)).tocsr()
-            X_offdi.sort_indices()
-
-            X = X_offdi+X_mdiag
-
-            # get L(X) and dX_L(X)
-            # get LT(X) and dX_LT(X)
-            T_violation_idx = (X_mdiag.data > 1.)
-            T_violation_cnt = np.sum(T_violation_idx)
-            T_violation_err = np.sum(X_mdiag.data[T_violation_idx] - 1.)
-            T_dX_A = scipy.sparse.diags(T_violation_idx.astype(float)).tocsr()
-            T_dX_I = scipy.sparse.diags(np.ones(self.K)*T_violation_cnt/self.K).tocsr()
-            T_dX = T_dX_A - T_dX_I
-
-            # get LI(X) and dX_LI(X)
-            HX = X_offdi.copy()
-            HX.data = ((X_offdi.data - 1.) * (Z - 1)) * self.H.data / Z
-            I = np.asarray(HX.sum(axis=1)).ravel()
-            I_max_I_sum = (self.I_max - (1.+ self.OFFSET)*self.I)
-            I_violation_idx = (I > I_max_I_sum)
-            I_violation_cnt = np.sum(I_violation_idx)
-            I_violation_err = np.sum((I[I_violation_idx] - I_max_I_sum[I_violation_idx])/self.I[I_violation_idx])
-            I_dX_A = csr_zero_rows_inplace(self.H.copy(),np.invert(I_violation_idx))
-            I_dX_A = csr_scal_rows_inplace(I_dX_A,1./self.I)
-            I_dX_A.data = I_dX_A.data*(Z-1.)/Z
-            I_dX_A = I_dX_A + I_dX_A.transpose()
-            I_dX_A.data = I_dX_A.data/2.
-            I_dX_B = scipy.sparse.diags(np.ones(self.K)*np.sum((-self.I[I_violation_idx]*(Z-1)/Z - I_max_I_sum[I_violation_idx])/self.I[I_violation_idx])/self.K).tocsr()
-            I_dX = I_dX_A+I_dX_B
-
-            # get LX(X) and dX_LX(X)
-            X_violation_idx = X_offdi.data < (-1. / (Z - 1.))
-            X_violation_cnt = np.sum(X_violation_idx)
-            X_violation_err = np.sum(X_offdi.data[X_violation_idx] * (1.-Z) - 1.)
-            X_dX_A = X_offdi.copy()
-            X_dX_A.data = np.zeros(X_dX_A.data.size)
-            X_dX_A.data[X_violation_idx] = - (Z-1.)
-            X_dX_A = X_dX_A + X_dX_A.transpose()
-            X_dX_A.data = X_dX_A.data/2.
-            X_dX_I = scipy.sparse.diags(np.ones(self.K)*X_violation_cnt/self.K).tocsr()
-            X_dX = X_dX_A-X_dX_I
-
-            sa_T, vh = scipy.sparse.linalg.eigsh(T_dX,k=1,which='LM')
-            sa_I, vh = scipy.sparse.linalg.eigsh(I_dX,k=1,which='LM')
-            sa_X, vh = scipy.sparse.linalg.eigsh(X_dX,k=1,which='LM')
-
-            print(i,"PHO_T_I_X",sa_T[0],sa_I[0],sa_X[0])
-            print(i,"ERR_T_I_X",T_violation_err,I_violation_err,X_violation_err)
-            print(i,"ECT_T_I_X",T_violation_cnt,I_violation_cnt,X_violation_cnt)
-            print(i,"ECT_T_I_X",T_violation_cnt/float(self.K),I_violation_cnt/float(self.K),X_violation_cnt/float(self.H.nnz))
-
-            dLX = scipy.sparse.csr_matrix((self.K, self.K))
-            LX = 0.
-            if T_violation_err > 0:
-                dLX = dLX + T_dX/np.abs(sa_T[0])
-                LX += T_violation_err/np.abs(sa_T[0])
-
-            if I_violation_err > 0:
-                dLX = dLX + I_dX/np.abs(sa_I[0])
-                LX += I_violation_err/np.abs(sa_I[0])
-
-            if X_violation_err > 0:
-                dLX = dLX + X_dX/np.abs(sa_X[0])
-                LX += X_violation_err/np.abs(sa_X[0])
-
-            sa, vh = scipy.sparse.linalg.eigsh(dLX,k=1,which='LM')
-            tmp_PHO = np.abs(sa[0])
-
-            print(i,"ERR_PHO_SUM",LX,tmp_PHO,"ERR/PHO", LX / tmp_PHO)
-
-            # update G
-            G = G  - (self.ETA / tmp_PHO) * dLX
-            d_sum = d_sum + LX / tmp_PHO
-
-            print(i,"ERR_AVG",d_sum/(i+1))
-            print(i,"X_DIA",X_mdiag_data[0:5])
-            print(i,"V_I_TRUE_CNT",np.sum((I[I_violation_idx]+self.I[I_violation_idx])/self.I_max[I_violation_idx] > 1.))
-            if LX<d_bst:
-                d_bst = LX
-
-            print(i,"BEST_ERR",d_bst)
-
-    @profile
     def run_fc_exphalffc_plug(self, Z, num_iterations=None, exphalffc=None) -> bool:
         assert exphalffc is not None
         if num_iterations is None:
@@ -157,8 +52,8 @@ class mmwm_scipy(feasibility_check_alg,StatusObject):
             X_offdi = scipy.sparse.coo_matrix((X_offdi_data, (self.H_x, self.H_y)), shape=(self.K, self.K)).tocsr()
             X_offdi.sort_indices()
 
-            print("min max X_offdi", np.max(X_offdi.data), np.min(X_offdi.data),np.argmax(X_offdi.data), np.argmin(X_offdi.data))
-            print("min max X_mdiag", np.max(X_mdiag.data), np.min(X_mdiag.data),np.argmax(X_mdiag.data), np.argmin(X_mdiag.data))
+            self._print(i,"min max X_offdi", np.max(X_offdi.data), np.min(X_offdi.data),np.argmax(X_offdi.data), np.argmin(X_offdi.data))
+            self._print(i,"min max X_mdiag", np.max(X_mdiag.data), np.min(X_mdiag.data),np.argmax(X_mdiag.data), np.argmin(X_mdiag.data))
             X = X_offdi+X_mdiag
 
             # get L(X) and dX_L(X)
@@ -186,26 +81,13 @@ class mmwm_scipy(feasibility_check_alg,StatusObject):
             I_dX_B = scipy.sparse.diags(np.ones(self.K)*np.sum((-self.I[I_violation_idx]*(Z-1)/Z - I_max_I_sum[I_violation_idx])/self.I[I_violation_idx])/self.K).tocsr()
             I_dX = I_dX_A+I_dX_B
 
-            # get LX(X) and dX_LX(X)
-            # X_violation_idx = X_offdi.data < (-1. / (Z - 1.))
-            # X_violation_cnt = np.sum(X_violation_idx)
-            # X_violation_err = np.sum(X_offdi.data[X_violation_idx] * (1.-Z) - 1.)
-            # X_dX_A = X_offdi.copy()
-            # X_dX_A.data = np.zeros(X_dX_A.data.size)
-            # X_dX_A.data[X_violation_idx] = - (Z-1.)
-            # X_dX_A = X_dX_A + X_dX_A.transpose()
-            # X_dX_A.data = X_dX_A.data/2.
-            # X_dX_I = scipy.sparse.diags(np.ones(self.K)*X_violation_cnt/self.K).tocsr()
-            # X_dX = X_dX_A-X_dX_I
-
             sa_T, vh = scipy.sparse.linalg.eigsh(T_dX,k=1,which='LM')
             sa_I, vh = scipy.sparse.linalg.eigsh(I_dX,k=1,which='LM')
-            # sa_X, vh = scipy.sparse.linalg.eigsh(X_dX,k=1,which='LM')
 
-            print(i,"PHO_T_I_X",sa_T[0],sa_I[0],0)
-            print(i,"ERR_T_I_X",T_violation_err,I_violation_err,0)
-            print(i,"ECT_T_I_X",T_violation_cnt,I_violation_cnt,0)
-            print(i,"V_I_TRUE_CNT",np.sum((I[I_violation_idx]+self.I[I_violation_idx])/self.I_max[I_violation_idx] > 1.))
+            self._print(i,"PHO_T_I_X",sa_T[0],sa_I[0])
+            self._print(i,"ERR_T_I_X",T_violation_err,I_violation_err)
+            self._print(i,"ECT_T_I_X",T_violation_cnt,I_violation_cnt)
+            self._print(i,"V_I_TRUE_CNT",np.sum((I[I_violation_idx]+self.I[I_violation_idx])/self.I_max[I_violation_idx] > 1.))
 
             dLX = scipy.sparse.csr_matrix((self.K, self.K))
             LX = 0.
@@ -217,231 +99,16 @@ class mmwm_scipy(feasibility_check_alg,StatusObject):
                 dLX = dLX + I_dX/np.abs(sa_I[0])*I_violation_err
                 LX += I_violation_err/np.abs(sa_I[0])*I_violation_err
 
-            # if X_violation_err > 0:
-            #     dLX = dLX + X_dX/np.abs(sa_X[0])
-            #     LX += X_violation_err/np.abs(sa_X[0])
-
             sa, vh = scipy.sparse.linalg.eigsh(dLX,k=1,which='LM')
             tmp_PHO = np.abs(sa[0])
-
 
             # update G
             G = G  - (self.ETA / tmp_PHO) * dLX
             d_sum = d_sum + LX / tmp_PHO
-
 
             gr, p = vec_rand_rounding.get_group_vec_using_ehalf_nattempt(Z,G_2.copy(),self.rxpr,self.I_max)
-            self._add_np_log("pct",np.asarray([p, p]),g_step=i)
+            self._add_np_log("pct",np.asarray(p),g_step=i)
 
-            #
-            # e_half = np.asarray(scipy.sparse.linalg.expm(G_2.tocsc()).todense())
-            #
-            # X_mdiag_data = np.sum(e_half * e_half, axis=1)
-            # X_trace = np.sum(X_mdiag_data)/self.K
-            # X_mdiag_data = X_mdiag_data/X_trace
-            # X_mdiag = scipy.sparse.diags(X_mdiag_data).tocsr()
-            # X_mdiag.sort_indices()
-            # X_offdi_data = np.sum(e_half[self.H_x] * e_half[self.H_y], axis=1)/X_trace
-            # X_offdi = scipy.sparse.coo_matrix((X_offdi_data, (self.H_x, self.H_y)), shape=(self.K, self.K)).tocsr()
-            # X_offdi.sort_indices()
-            #
-            #
-            # print("min max X_offdi", np.max(X_offdi.data), np.min(X_offdi.data),np.argmax(X_offdi.data), np.argmin(X_offdi.data))
-            # print("min max X_mdiag", np.max(X_mdiag.data), np.min(X_mdiag.data),np.argmax(X_mdiag.data), np.argmin(X_mdiag.data))
-            # X = X_offdi+X_mdiag
-            #
-            # # get L(X) and dX_L(X)
-            # # get LT(X) and dX_LT(X)
-            # T_violation_idx = (X_mdiag.data > 1.)
-            # T_violation_cnt = np.sum(T_violation_idx)
-            # T_violation_err = np.sum(X_mdiag.data[T_violation_idx] - 1.)
-            # T_dX_A = scipy.sparse.diags(T_violation_idx.astype(float)).tocsr()
-            # T_dX_I = scipy.sparse.diags(np.ones(self.K)*T_violation_cnt/self.K).tocsr()
-            # T_dX = T_dX_A - T_dX_I
-            #
-            # # get LI(X) and dX_LI(X)
-            # HX = X_offdi.copy()
-            # HX.data = ((X_offdi.data - 1.) * (Z - 1)) * self.H.data / Z
-            # I = np.asarray(HX.sum(axis=1)).ravel()
-            # I_max_I_sum = (self.I_max - (1.+ self.OFFSET)*self.I)
-            # I_violation_idx = (I > I_max_I_sum)
-            # I_violation_cnt = np.sum(I_violation_idx)
-            # I_violation_err = np.sum((I[I_violation_idx] - I_max_I_sum[I_violation_idx])/self.I[I_violation_idx])
-            # I_dX_A = csr_zero_rows_inplace(self.H.copy(),np.invert(I_violation_idx))
-            # I_dX_A = csr_scal_rows_inplace(I_dX_A,1./self.I)
-            # I_dX_A.data = I_dX_A.data*(Z-1.)/Z
-            # I_dX_A = I_dX_A + I_dX_A.transpose()
-            # I_dX_A.data = I_dX_A.data/2.
-            # I_dX_B = scipy.sparse.diags(np.ones(self.K)*np.sum((-self.I[I_violation_idx]*(Z-1)/Z - I_max_I_sum[I_violation_idx])/self.I[I_violation_idx])/self.K).tocsr()
-            # I_dX = I_dX_A+I_dX_B
-            #
-            # # get LX(X) and dX_LX(X)
-            # X_violation_idx = X_offdi.data < (-1. / (Z - 1.))
-            # X_violation_cnt = np.sum(X_violation_idx)
-            # X_violation_err = np.sum(X_offdi.data[X_violation_idx] * (1.-Z) - 1.)
-            # X_dX_A = X_offdi.copy()
-            # X_dX_A.data = np.zeros(X_dX_A.data.size)
-            # X_dX_A.data[X_violation_idx] = - (Z-1.)
-            # X_dX_A = X_dX_A + X_dX_A.transpose()
-            # X_dX_A.data = X_dX_A.data/2.
-            # X_dX_I = scipy.sparse.diags(np.ones(self.K)*X_violation_cnt/self.K).tocsr()
-            # X_dX = X_dX_A-X_dX_I
-            #
-            # print(i,"ERR_T_I_X",T_violation_err,I_violation_err,X_violation_err)
-            # print(i,"ECT_T_I_X",T_violation_cnt,I_violation_cnt,X_violation_cnt)
-            # print(i,"V_I_TRUE_CNT",np.sum((I[I_violation_idx]+self.I[I_violation_idx])/self.I_max[I_violation_idx] > 1.))
-
-    @profile
-    def run_fc_exphalffc_plug_indp(self, Z, num_iterations=None, exphalffc=None) -> bool:
-        assert exphalffc is not None
-        if num_iterations is None:
-            num_iterations = self._get_niteration()
-
-        d_sum = 0
-
-        G = scipy.sparse.csr_matrix((self.K, self.K))
-        G_pre = scipy.sparse.csr_matrix((self.K, self.K))
-        for i in range(num_iterations):
-            # compute X from G
-            G_2 = G_pre.copy()
-            G_2.data = G_2.data/2.
-            e_half = exphalffc(G_2.copy())
-
-            X_mdiag_data = np.sum(e_half * e_half, axis=1)
-            X_trace = np.sum(X_mdiag_data)/self.K
-            X_mdiag_data = X_mdiag_data/X_trace
-            X_mdiag = scipy.sparse.diags(X_mdiag_data).tocsr()
-            X_mdiag.sort_indices()
-            X_offdi_data = np.sum(e_half[self.H_x] * e_half[self.H_y], axis=1)/X_trace
-            X_offdi = scipy.sparse.coo_matrix((X_offdi_data, (self.H_x, self.H_y)), shape=(self.K, self.K)).tocsr()
-            X_offdi.sort_indices()
-
-            X = X_offdi+X_mdiag
-
-            # get L(X) and dX_L(X)
-            # get LT(X) and dX_LT(X)
-            T_violation_idx = (X_mdiag.data > 1.)
-            T_violation_cnt = np.sum(T_violation_idx)
-            T_violation_err = np.sum(X_mdiag.data[T_violation_idx] - 1.)
-            T_dX_A = scipy.sparse.diags(T_violation_idx.astype(float)).tocsr()
-            T_dX_I = scipy.sparse.diags(np.ones(self.K)*T_violation_cnt/self.K).tocsr()
-            T_dX = T_dX_A - T_dX_I
-
-            # get LI(X) and dX_LI(X)
-            HX = X_offdi.copy()
-            HX.data = ((X_offdi.data - 1.) * (Z - 1)) * self.H.data / Z
-            I = np.asarray(HX.sum(axis=1)).ravel()
-            I_max_I_sum = (self.I_max - (1.+ self.OFFSET)*self.I)
-            I_violation_idx = (I > I_max_I_sum)
-            I_violation_cnt = np.sum(I_violation_idx)
-            I_violation_err = np.sum((I[I_violation_idx] - I_max_I_sum[I_violation_idx])/self.I[I_violation_idx])
-            I_dX_A = csr_zero_rows_inplace(self.H.copy(),np.invert(I_violation_idx))
-            I_dX_A = csr_scal_rows_inplace(I_dX_A,1./self.I)
-            I_dX_A.data = I_dX_A.data*(Z-1.)/Z
-            I_dX_A = I_dX_A + I_dX_A.transpose()
-            I_dX_A.data = I_dX_A.data/2.
-            I_dX_B = scipy.sparse.diags(np.ones(self.K)*np.sum((-self.I[I_violation_idx]*(Z-1)/Z - I_max_I_sum[I_violation_idx])/self.I[I_violation_idx])/self.K).tocsr()
-            I_dX = I_dX_A+I_dX_B
-
-            # get LX(X) and dX_LX(X)
-            X_violation_idx = X_offdi.data < (-1. / (Z - 1.))
-            X_violation_cnt = np.sum(X_violation_idx)
-            X_violation_err = np.sum(X_offdi.data[X_violation_idx] * (1.-Z) - 1.)
-            X_dX_A = X_offdi.copy()
-            X_dX_A.data = np.zeros(X_dX_A.data.size)
-            X_dX_A.data[X_violation_idx] = - (Z-1.)
-            X_dX_A = X_dX_A + X_dX_A.transpose()
-            X_dX_A.data = X_dX_A.data/2.
-            X_dX_I = scipy.sparse.diags(np.ones(self.K)*X_violation_cnt/self.K).tocsr()
-            X_dX = X_dX_A-X_dX_I
-
-            sa_T, vh = scipy.sparse.linalg.eigsh(T_dX,k=1,which='LM')
-            sa_I, vh = scipy.sparse.linalg.eigsh(I_dX,k=1,which='LM')
-            sa_X, vh = scipy.sparse.linalg.eigsh(X_dX,k=1,which='LM')
-
-            print(i,"ERR_T_I_X",T_violation_err,I_violation_err,X_violation_err)
-            print(i,"ECT_T_I_X",T_violation_cnt,I_violation_cnt,X_violation_cnt)
-            print(i,"V_I_TRUE_CNT",np.sum((I[I_violation_idx]+self.I[I_violation_idx])/self.I_max[I_violation_idx] > 1.))
-
-            dLX = scipy.sparse.csr_matrix((self.K, self.K))
-            LX = 0.
-            if T_violation_err > 0:
-                dLX = dLX + T_dX/np.abs(sa_T[0])
-                LX += T_violation_err/np.abs(sa_T[0])
-
-            if I_violation_err > 0:
-                dLX = dLX + I_dX/np.abs(sa_I[0])
-                LX += I_violation_err/np.abs(sa_I[0])
-
-            if X_violation_err > 0:
-                dLX = dLX + X_dX/np.abs(sa_X[0])
-                LX += X_violation_err/np.abs(sa_X[0])
-
-            sa, vh = scipy.sparse.linalg.eigsh(dLX,k=1,which='LM')
-            tmp_PHO = np.abs(sa[0])
-
-
-            # update G
-            G_pre = G.copy()
-            G = G  - (self.ETA / tmp_PHO) * dLX
-            d_sum = d_sum + LX / tmp_PHO
-
-
-            e_half = np.asarray(scipy.sparse.linalg.expm(G_2.tocsc()).todense())
-
-            X_mdiag_data = np.sum(e_half * e_half, axis=1)
-            X_trace = np.sum(X_mdiag_data)/self.K
-            X_mdiag_data = X_mdiag_data/X_trace
-            X_mdiag = scipy.sparse.diags(X_mdiag_data).tocsr()
-            X_mdiag.sort_indices()
-            X_offdi_data = np.sum(e_half[self.H_x] * e_half[self.H_y], axis=1)/X_trace
-            X_offdi = scipy.sparse.coo_matrix((X_offdi_data, (self.H_x, self.H_y)), shape=(self.K, self.K)).tocsr()
-            X_offdi.sort_indices()
-
-            X = X_offdi+X_mdiag
-
-            # get L(X) and dX_L(X)
-            # get LT(X) and dX_LT(X)
-            T_violation_idx = (X_mdiag.data > 1.)
-            T_violation_cnt = np.sum(T_violation_idx)
-            T_violation_err = np.sum(X_mdiag.data[T_violation_idx] - 1.)
-            T_dX_A = scipy.sparse.diags(T_violation_idx.astype(float)).tocsr()
-            T_dX_I = scipy.sparse.diags(np.ones(self.K)*T_violation_cnt/self.K).tocsr()
-            T_dX = T_dX_A - T_dX_I
-
-            # get LI(X) and dX_LI(X)
-            HX = X_offdi.copy()
-            HX.data = ((X_offdi.data - 1.) * (Z - 1)) * self.H.data / Z
-            I = np.asarray(HX.sum(axis=1)).ravel()
-            I_max_I_sum = (self.I_max - (1.+ self.OFFSET)*self.I)
-            I_violation_idx = (I > I_max_I_sum)
-            I_violation_cnt = np.sum(I_violation_idx)
-            I_violation_err = np.sum((I[I_violation_idx] - I_max_I_sum[I_violation_idx])/self.I[I_violation_idx])
-            I_dX_A = csr_zero_rows_inplace(self.H.copy(),np.invert(I_violation_idx))
-            I_dX_A = csr_scal_rows_inplace(I_dX_A,1./self.I)
-            I_dX_A.data = I_dX_A.data*(Z-1.)/Z
-            I_dX_A = I_dX_A + I_dX_A.transpose()
-            I_dX_A.data = I_dX_A.data/2.
-            I_dX_B = scipy.sparse.diags(np.ones(self.K)*np.sum((-self.I[I_violation_idx]*(Z-1)/Z - I_max_I_sum[I_violation_idx])/self.I[I_violation_idx])/self.K).tocsr()
-            I_dX = I_dX_A+I_dX_B
-
-            # get LX(X) and dX_LX(X)
-            X_violation_idx = X_offdi.data < (-1. / (Z - 1.))
-            X_violation_cnt = np.sum(X_violation_idx)
-            X_violation_err = np.sum(X_offdi.data[X_violation_idx] * (1.-Z) - 1.)
-            X_dX_A = X_offdi.copy()
-            X_dX_A.data = np.zeros(X_dX_A.data.size)
-            X_dX_A.data[X_violation_idx] = - (Z-1.)
-            X_dX_A = X_dX_A + X_dX_A.transpose()
-            X_dX_A.data = X_dX_A.data/2.
-            X_dX_I = scipy.sparse.diags(np.ones(self.K)*X_violation_cnt/self.K).tocsr()
-            X_dX = X_dX_A-X_dX_I
-
-            print(i,"ERR_T_I_X",T_violation_err,I_violation_err,X_violation_err)
-            print(i,"ECT_T_I_X",T_violation_cnt,I_violation_cnt,X_violation_cnt)
-            print(i,"V_I_TRUE_CNT",np.sum((I[I_violation_idx]+self.I[I_violation_idx])/self.I_max[I_violation_idx] > 1.))
-            idx = (I[I_violation_idx]+self.I[I_violation_idx])/self.I_max[I_violation_idx] > 1.
-            print(i,"V_I_TRUE_CNT",((I[I_violation_idx]+self.I[I_violation_idx])/self.I_max[I_violation_idx])[idx])
 
     def set_st(self, rxpr):
         self._process_state(rxpr)
