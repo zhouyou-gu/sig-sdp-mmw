6de7bb6bb214d5564790e4da1686bae11638da7e
zhouyou.gu <zhouyou.gu@sydney.edu.au>
2024-07-22 06:13:17 +1000

    add bler tests

---
+++ sim_script/journal_version/plot_data_bler_avg_max.py
@@ -0,0 +1,102 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sim_src.util import GET_LOG_PATH_FOR_SIM_SCRIPT
+
+FONT_SIZE = 9
+fig_width_px = 350
+fig_height_px = 225
+dpi = 100  # Typical screen DPI, adjust if necessary
+fig_width_in = fig_width_px / dpi
+fig_height_in = fig_height_px / dpi
+
+plt.rc('font', family='serif')
+plt.rc('mathtext', fontset='cm')
+plt.rc('font', size=FONT_SIZE)  # Default font size
+plt.rc('axes', titlesize=FONT_SIZE)  # Font size of the axes title
+plt.rc('axes', labelsize=FONT_SIZE)  # Font size of the x and y labels
+plt.rc('xtick', labelsize=FONT_SIZE)  # Font size of the tick labels
+plt.rc('ytick', labelsize=FONT_SIZE)  # Font size of the tick labels
+plt.rc('legend', fontsize=FONT_SIZE)  # Font size for legends
+
+
+data_name_list = ["MMW","LP","MINTP","MASSO"]
+file_name_list = ["mmw","ladmm","mgain","masso"]
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Plot the data
+fig, axs = plt.subplots(1,2,)
+fig.set_size_inches(fig_width_in, fig_height_in)  # 3.5 inches width, height adjusted to maintain aspect ratio
+
+N_REPEAT = 100
+N_POINTS = 11
+# Plot settings
+markers = ['o', 's', '^','+']  # Different markers for each line
+p_names = [r'a',r'b']
+lines = []
+labels = []
+for a in [0]:
+    for ss in range(len(file_name_list)):
+        s = file_name_list[ss]
+        data_point = np.zeros(N_POINTS)
+        for t in range(N_POINTS):
+            fname = s+"-"+str(t+5)+"-"+"75"
+            data_file = os.path.join(current_dir, "sim_all_bler","sim_all_bler-2024-July-21-02-32-37-ail",fname)
+            data = np.genfromtxt(data_file, delimiter=',')
+            data = data[:, 3]
+            data_point[t] = np.mean(data)
+        line, = axs[a].plot(np.arange(5,5+N_POINTS)*20, data_point, marker=markers[ss],linewidth=1,markerfacecolor='none')
+        lines.append(line)
+        axs[a].set_position([0.16+a*0.455, 0.215, 0.35, 0.6])
+        # Add labels and title
+        axs[a].set_xlabel(r'WTSN size $l$ m')
+        axs[a].text(0.75, 0.1, p_names[a], transform=axs[a].transAxes, fontsize=FONT_SIZE)
+        axs[a].grid(True)
+
+for a in [1]:
+    for ss in range(len(file_name_list)):
+        s = file_name_list[ss]
+        data_point = np.zeros(N_POINTS)
+        for t in range(N_POINTS):
+            fname = s+"-"+str(t+5)+"-"+"75"
+            data_file = os.path.join(current_dir, "sim_all_bler","sim_all_bler-2024-July-21-02-32-37-ail",fname)
+            data = np.genfromtxt(data_file, delimiter=',')
+            data = data[:, 4]
+            data_point[t] = np.mean(data)
+        line, = axs[a].plot(np.arange(5,5+N_POINTS)*20, data_point, marker=markers[ss],linewidth=1,markerfacecolor='none')
+        lines.append(line)
+        axs[a].set_position([0.16+a*0.455, 0.215, 0.35, 0.6])
+        # Add labels and title
+        axs[a].set_xlabel(r'WTSN size $l$ m')
+        axs[a].text(0.75, 0.1, p_names[a], transform=axs[a].transAxes, fontsize=FONT_SIZE)
+        axs[a].grid(True)
+
+axs[0].set_ylabel(r'Error rates')
+# uu = 5*20
+# ll = 15*20
+axs[0].set_xlim(5*20, 15*20)
+axs[1].set_xlim(5*20, 15*20)
+
+# axs[1].set_xlim(uu, ll)
+# axs[2].set_xlim(uu, ll)
+axs[0].set_ylim(1e-5, 1)
+axs[1].set_ylim(1e-5, 1)
+# axs[1].set_yticks([])
+axs[0].set_yscale('log')
+axs[1].set_yscale('log')
+
+
+# Add a legend
+fig.legend(lines[0:4], data_name_list ,fontsize=FONT_SIZE, loc='lower left', bbox_to_anchor=(0.16, 0.85, 0.805, 0.1), mode="expand",ncol = 4 ,borderaxespad=0.1,handlelength=1)
+# axs[0].legend(fontsize=8, loc='lower left', bbox_to_anchor=(0, 1.02, 5,0.1), ncol=3,borderaxespad=0.)
+# plt.subplots_adjust(left=0.175, right=0.95,bottom=0.175,top=0.95)
+
+
+# Save the figure as a PDF
+output_path = os.path.join(current_dir, os.path.splitext(os.path.basename(__file__))[0]) + '.pdf'
+
+fig.savefig(output_path, format='pdf', bbox_inches='tight', pad_inches=0.)
+
+# Display the plot
+plt.show()
--- sim_script/journal_version/sim_all_bler.py
+++ sim_script/journal_version/sim_all_bler.py
@@ -23,24 +23,29 @@ log = CSV_WRITER_OBJECT(path=log_path)
 np.set_printoptions(threshold=10)
 np.set_printoptions(linewidth=1000)
 
-REPEAT =  20
+REPEAT =  100
 
-RHO =  5e-3
+RHO =  75e-4
 
 for CELL_SIZE in range(5,16):
     for seed in range(REPEAT):
         e = env(cell_size=CELL_SIZE,sta_density_per_1m2=RHO,seed=seed)
         bs = binary_search_relaxation()
-        bs.force_lower_bound = False
-        alg = admm_sdp_solver(nit=100)
+        alg = mmw(nit=150,eta=0.04)
         bs.feasibility_check_alg = alg
         z_vec, Z_fin, remainder = bs.run(e.generate_S_Q_hmax())
         bler = e.evaluate_bler(z_vec, Z_fin)
         mbler = np.mean(bler)
         wbler = np.max(bler)
-        log.log_mul_scalar(data_name="sadmm-"+str(CELL_SIZE)+"-"+str(int(RHO*10000)),iteration=seed,values=[Z_fin,mbler,wbler]+bler.tolist())
+        log.log_mul_scalar(data_name="mmw-"+str(CELL_SIZE)+"-"+str(int(RHO*10000)),iteration=seed,values=[Z_fin,mbler,wbler])
 
-
+        alg = admm_sdp_solver(nit=1000)
+        _, gX = alg.run_with_state(0,Z_fin,e.generate_S_Q_hmax())
+        z_vec, _, _ = alg.rounding(Z_fin,gX,e.generate_S_Q_hmax())
+        bler = e.evaluate_bler(z_vec, Z_fin)
+        mbler = np.mean(bler)
+        wbler = np.max(bler)
+        log.log_mul_scalar(data_name="sadmm-"+str(CELL_SIZE)+"-"+str(int(RHO*10000)),iteration=seed,values=[Z_fin,mbler,wbler])
 
         alg = lrp_solver(nit=100)
         _, gX = alg.run_with_state(0,Z_fin,e.generate_S_Q_hmax())
@@ -48,16 +53,16 @@ for CELL_SIZE in range(5,16):
         bler = e.evaluate_bler(z_vec, Z_fin)
         mbler = np.mean(bler)
         wbler = np.max(bler)
-        log.log_mul_scalar(data_name="ladmm-"+str(CELL_SIZE)+"-"+str(int(RHO*10000)),iteration=seed,values=[Z_fin,mbler,wbler]+bler.tolist())
+        log.log_mul_scalar(data_name="ladmm-"+str(CELL_SIZE)+"-"+str(int(RHO*10000)),iteration=seed,values=[Z_fin,mbler,wbler])
 
         z_vec, _, _ = MAX_GAIN.run(Z_fin,state=(e.generate_S_Q_hmax()))
         bler = e.evaluate_bler(z_vec, Z_fin)
         mbler = np.mean(bler)
         wbler = np.max(bler)
-        log.log_mul_scalar(data_name="mgain-"+str(CELL_SIZE)+"-"+str(int(RHO*10000)),iteration=seed,values=[Z_fin,mbler,wbler]+bler.tolist())
+        log.log_mul_scalar(data_name="mgain-"+str(CELL_SIZE)+"-"+str(int(RHO*10000)),iteration=seed,values=[Z_fin,mbler,wbler])
 
         z_vec, _, _ = MAX_ASSO.run(Z_fin,state=(e.generate_S_Q_hmax()))
         bler = e.evaluate_bler(z_vec, Z_fin)
         mbler = np.mean(bler)
         wbler = np.max(bler)
-        log.log_mul_scalar(data_name="masso-"+str(CELL_SIZE)+"-"+str(int(RHO*10000)),iteration=seed,values=[Z_fin,mbler,wbler]+bler.tolist())
+        log.log_mul_scalar(data_name="masso-"+str(CELL_SIZE)+"-"+str(int(RHO*10000)),iteration=seed,values=[Z_fin,mbler,wbler])
