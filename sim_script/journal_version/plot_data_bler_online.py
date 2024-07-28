215bba85d6cd70f8ba615a24658617369c353e89
zhouyou.gu <zhouyou.gu@sydney.edu.au>
2024-07-28 19:14:54 +1000

    update simulation file

--- sim_script/journal_version/plot_data_bler_online.py
+++ sim_script/journal_version/plot_data_bler_online.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from sim_src.util import GET_LOG_PATH_FOR_SIM_SCRIPT
 
 FONT_SIZE = 9
-fig_width_px = 300
+fig_width_px = 350
 fig_height_px = 250
 dpi = 100  # Typical screen DPI, adjust if necessary
 fig_width_in = fig_width_px / dpi
@@ -21,7 +21,7 @@ plt.rc('ytick', labelsize=FONT_SIZE)  # Font size of the tick labels
 plt.rc('legend', fontsize=FONT_SIZE)  # Font size for legends
 
 
-data_name_list = [r'Heuristic',r'Proposed']
+data_name_list = [r'Heuristic',r'Proposed',r'Ideal']
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Plot the data
@@ -43,24 +43,24 @@ cell_size_list = [10]
 bars = []
 
 for a in [0]:
-    file_name_list = ["mgain","mmw"]
+    file_name_list = ["mgain","mmw","ideal"]
     for tt in range(len(file_name_list)):
         t = file_name_list[tt]
         datas = np.zeros((100,11))
         ratios = np.zeros((100,11))
         for ss in range(11):
             fname = "online"+"-"+t+"-"+str(ss)+"-"+str(150)+"-"+str(cell_size_list[a])+"-"+"75"
-            data_file = os.path.join(current_dir, "sim_mmw_online","sim_mmw_online-2024-July-27-19-17-21-ail",fname)
+            data_file = os.path.join(current_dir, "sim_mmw_online","sim_mmw_online-2024-July-28-16-46-38-ail",fname)
             data = np.genfromtxt(data_file, delimiter=',')
             data = np.mean(data[:, 2:],axis=1)
             datas[:,ss] = data
             ratios[:,ss] = datas[:,ss]/datas[:,0]
             # cdf = np.arange(1, len(data) + 1) / len(data)
-        index = np.arange(6)*2
-        bar_width = 0.5
-        b = axs[a].bar(index + tt * bar_width - (len(data_name_list)-1)*bar_width/2., np.mean(datas,axis=0)[0:11:2], bar_width, label=file_name_list[tt],color=colors[tt])
+        index = np.arange(5)*2
+        bar_width = 0.4
+        b = axs[a].bar(index + tt * bar_width - (len(data_name_list)-1)*bar_width/2., np.mean(datas,axis=0)[2:11:2], bar_width, label=file_name_list[tt],color=colors[tt])
         bars.append(b)
-    axs[a].set_position([0.2, 0.2+a*0.4, 0.775, 0.6])
+    axs[a].set_position([0.2, 0.2+a*0.4, 0.775, 0.625])
     # Add labels and title
     # axs.text(0.75, 0.1, p_names[a], transform=axs[a].transAxes, fontsize=FONT_SIZE)
     axs[a].grid(True)
@@ -76,12 +76,14 @@ axs[0].set_xlabel(r'User mobility (meter/second)')
 # # axs[1].set_xlim(uu, ll)
 # # # axs[2].set_xlim(uu, ll)
 # axs[a].set_xlim(-0.2, 2)
-axs[0].set_xticks(np.arange(6)*2)
-axs[0].set_xticklabels(np.arange(6)*2/10.)
-#
+axs[0].set_xticks(np.arange(5)*2)
+axs[0].set_xticklabels(["0.2","0.4","0.6","0.8","1.0"])
+from matplotlib.ticker import FormatStrFormatter
+# axs[0].xaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+
 # axs[1].set_xlim(9, 18)
 # axs[1].set_xticks([9,12,15,18])
-axs[0].set_ylim(1e-4, 5e-2)
+axs[0].set_ylim(1e-3, 2e-2)
 # axs[0].set_yticks([0.8,0.9,1])
 # axs[1].set_ylim(0, 1)
 # axs[1].set_yticks([0.,0.5,1])
@@ -97,7 +99,7 @@ axs[0].set_yscale('log')
 
 
 # Add a legend
-fig.legend(bars[0:len(data_name_list)], data_name_list ,fontsize=FONT_SIZE, loc='lower left', bbox_to_anchor=(0.2, 0.85, 0.775, 0.1), mode="expand",ncol = 2 ,borderaxespad=0.1,handlelength=1.5, handleheight= 1)
+fig.legend(bars[0:len(data_name_list)], data_name_list ,fontsize=FONT_SIZE, loc='lower left', bbox_to_anchor=(0.2, 0.835, 0.775, 0.3), mode="expand",ncol = 3 ,borderaxespad=0.1,handlelength=1.5, handleheight= 1)
 # axs[0].legend(fontsize=8, loc='lower left', bbox_to_anchor=(0, 1.02, 5,0.1), ncol=3,borderaxespad=0.)
 # plt.subplots_adjust(left=0.175, right=0.95,bottom=0.175,top=0.95)
 
